81067fe1f2d7c4cbe6f22163fe6e6fe79ea0adbc
ethanmusser <ethanmusser1@gmail.com>
2021-12-13 20:38:00 -0500

    Initial commit for esp32-master

---
+++ esp32-master/esp32-master.ino
@@ -0,0 +1,15 @@
+/**
+ * esp32-master.ino
+ * Code for the master ESP32 Pico Kit used for sensing and coordination.
+ * 
+ * Grand Theft Autonomous -- Group 28
+ * Mechatronics (MEAM 510)
+ * 
+ * @author Ethan J. Musser
+ * @version 0.1
+ */
+
+
+void setup() {}
+
+void loop() {}
