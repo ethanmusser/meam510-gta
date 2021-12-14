44e4df6d4d4885d9cc0de6f79d955a668ffcbb08
ethanmusser <ethanmusser1@gmail.com>
2021-12-13 19:56:29 -0500

    Implements MecanumBase class

--- esp32-slave/MecanumBase.cpp
+++ esp32-slave/MecanumBase.cpp
@@ -21,3 +21,48 @@ MecanumBase::MecanumBase(MotorController& frontLeftMotor,
       _rearRightMotor(&rearRightMotor)
 {}
 
+void MecanumBase::drivePolar(float magnitude, 
+                             float angle,
+                             float zRotation)
+{
+    // Clamp Inputs
+    magnitude = constrain(magnitude, -1.0, 1.0);
+    // angle = constrain(angle, 0.0, 2.0*M_PI);
+    zRotation = constrain(zRotation, -1.0, 1.0);
+    
+    // Compute Wheel Speeds
+    float trigEntry = angle + M_PI_4;
+    WheelSpeeds wheelSpeeds;
+    wheelSpeeds.frontLeftSpeed  = magnitude * sin(trigEntry) + zRotation;
+    wheelSpeeds.frontRightSpeed = magnitude * cos(trigEntry) - zRotation;
+    wheelSpeeds.rearLeftSpeed   = magnitude * cos(trigEntry) + zRotation;
+    wheelSpeeds.rearRightSpeed  = magnitude * sin(trigEntry) - zRotation;
+
+    // Set Wheel Speeds
+    setWheelSpeeds(wheelSpeeds);
+}
+
+void MecanumBase::stop()
+{
+    _frontLeftMotor->stop();
+    _frontRightMotor->stop();
+    _rearLeftMotor->stop();
+    _rearRightMotor->stop();
+}
+
+void MecanumBase::brake()
+{
+    _frontLeftMotor->brake();
+    _frontRightMotor->brake();
+    _rearLeftMotor->brake();
+    _rearRightMotor->brake();
+}
+
+void MecanumBase::setWheelSpeeds(WheelSpeeds speeds)
+{
+    _wheelSpeeds = speeds;
+    _frontLeftMotor->set(_wheelSpeeds.frontLeftSpeed);
+    _frontRightMotor->set(_wheelSpeeds.frontRightSpeed);
+    _rearLeftMotor->set(_wheelSpeeds.rearLeftSpeed);
+    _rearRightMotor->set(_wheelSpeeds.rearRightSpeed);
+}
